f531b88621886d5a10d76f5ccbf65e6540e01470
Đức Long <duclongsew@gmail.com>
2022-01-17 21:22:42 +0700

    Add api -update template -create Api

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+import axiosClient from "./api/axiosClient"
+import postApi from "./api/postApi"
+
 console.log("long")
+
+async function main(){
+    const queryParams = {
+        _page: 1,
+        _limit: 5,
+    }
+    const reponsive = await postApi.getAll(queryParams)
+    console.log(reponsive)
+}
+main()
