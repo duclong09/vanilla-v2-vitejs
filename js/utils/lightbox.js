29387e40a1235a5960e68a77e7ac13157f2d434a
Đức Long <duclongsew@gmail.com>
2022-02-15 09:44:41 +0700

    Show full modal

--- js/utils/lightbox.js
+++ js/utils/lightbox.js
@@ -1,56 +1,58 @@
-
-function showModal(modalElement){
-    if(!window.bootstrap) return
+function showModal(modalElement) {
+    if (!window.bootstrap) return
     const modal = new window.bootstrap.Modal(modalElement)
-    if(modal) modal.show()
+    if (modal) modal.show()
 }
 
-export function registerLightBox({modalId,imgSelector,prevSelector,nextSelector}){
+export function registerLightBox({ modalId, imgSelector, prevSelector, nextSelector }) {
+    
     const modalElement = document.getElementById(modalId)
-    if(!modalElement) return
+    if (!modalElement) return
+    if(Boolean(modalElement.dataset.registered)) return
     //selectors
     const imageElement = modalElement.querySelector(imgSelector)
     const prevButton = modalElement.querySelector(prevSelector)
     const nextButton = modalElement.querySelector(nextSelector)
 
-    if(!imageElement || !prevButton || !nextButton) return
+    if (!imageElement || !prevButton || !nextButton) return
 
     //lightbox vars
     let imgList = []
     let currentIndex = 0
 
-
-    function showImageAtIndex(index){
+    //determine index of selected img
+    function showImageAtIndex(index) {
         imageElement.src = imgList[index].src
     }
 
     //hander click for all imgs -> Event delegation
-    document.addEventListener('click',(event) => {
+    document.addEventListener('click', (event) => {
         //console.log('click', event.target)
-        const {target} = event
-        if(target.tagName !== 'IMG' || !target.dataset.album) return
+        const { target } = event
+        if (target.tagName !== 'IMG' || !target.dataset.album) return
         //img click -> find imgs with the same album / gallery
         imgList = document.querySelectorAll(`img[data-album="${target.dataset.album}"]`)
-        currentIndex = [ ... imgList].findIndex( x => x === target)
-        console.log('alnum image click', target,currentIndex, imgList)
-
+        currentIndex = [...imgList].findIndex((x) => x === target)
+        //console.log('alnum image click', target,currentIndex, imgList)
+        //show modal with selected img
         //show img index
         showImageAtIndex(currentIndex)
         //show modal
         showModal(modalElement)
     })
 
+    //handle prev / next click
     prevButton.addEventListener('click', () => {
         //show prev image of current album
+        currentIndex = (currentIndex - 1 + imgList.length) % imgList.length
+        showImageAtIndex(currentIndex)
     })
 
     nextButton.addEventListener('click', () => {
         //show next image of current album
+        currentIndex = (currentIndex + 1) % imgList.length
+        showImageAtIndex(currentIndex)
     })
-   
-    //determine index of selected img
-
-    //show modal with selected img
-
-    //handle prev / next click
+    //mark this modal
+    modalElement.dataset.registered = 'true'
 }
