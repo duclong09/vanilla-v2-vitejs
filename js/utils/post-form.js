efa4f9d1cc97a0d05e4b5a1fc7dcb0c4b89dae25
Đức Long <duclongsew@gmail.com>
2022-02-17 15:12:17 +0700

    Add Yup package -Validation string -Schema -Post Schema -Validation Error

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -1,96 +1,135 @@
+import { setBackgroundImage, setTextContent } from './common'
+import { setFieldValue } from './common'
+import * as yup from 'yup'
 
-import { setBackgroundImage, setTextContent } from "./common"
-import { setFieldValue } from "./common"
-
-function setFormValues(form, formValues){
-    setFieldValue(form, '[name="title"]', formValues?.title)
-    setFieldValue(form, '[name="author"]', formValues?.author)
-    setFieldValue(form, '[name="description"]', formValues?.description)
-    //hiden field
-    setFieldValue(form, '[name="imageUrl"]', formValues?.imageUrl)
-    setBackgroundImage(document, '#postHeroImage', formValues?.imageUrl)
+function setFormValues(form, formValues) {
+  setFieldValue(form, '[name="title"]', formValues?.title)
+  setFieldValue(form, '[name="author"]', formValues?.author)
+  setFieldValue(form, '[name="description"]', formValues?.description)
+  //hiden field
+  setFieldValue(form, '[name="imageUrl"]', formValues?.imageUrl)
+  setBackgroundImage(document, '#postHeroImage', formValues?.imageUrl)
 }
-
 
 //create form value
-function getFormValues(form){
-    const formValues = {}; 
-    //v1 : query each input and add to values object
-    // ['title', 'author', 'description', 'imageUrl'].forEach((name) => {
-    //     const field = form.querySelector(`[name="${name}"]`)
-    //     if(field) formValues[name] = field.value
-    // })
+function getFormValues(form) {
+  const formValues = {}
+  //v1 : query each input and add to values object
+  // ['title', 'author', 'description', 'imageUrl'].forEach((name) => {
+  //     const field = form.querySelector(`[name="${name}"]`)
+  //     if(field) formValues[name] = field.value
+  // })
 
-    //v2: using form data
+  //v2: using form data
 
-    const data = new FormData(form)
-    for(const[key,value] of data){
-        formValues[key] = value
-    }
-    return formValues
+  const data = new FormData(form)
+  for (const [key, value] of data) {
+    formValues[key] = value
+  }
+  return formValues
 }
 
-function getTitleError(form){
-    const titleElement = form.querySelector('[name="title"]')
-    if(!titleElement) return
-    if(titleElement.validity.valueMissing) return 'Ooops! Please write a nice title for your post'
+// function getTitleError(form) {
+//   const titleElement = form.querySelector('[name="title"]')
+//   if (!titleElement) return
+//   if (titleElement.validity.valueMissing) return 'Ooops! Please write a nice title for your post'
 
-    if(titleElement.value.split(' ').filter((x) => !! x && x.length >= 3).length < 2){
-        return 'Please enter at least two words of 3 character'
-    }
-    return ''
+//   if (titleElement.value.split(' ').filter((x) => !!x && x.length >= 3).length < 2) {
+//     return 'Please enter at least two words of 3 character'
+//   }
+//   return ''
+// }
+
+// function getAuthorError(form) {
+//   const authorElement = form.querySelector('[name="author"]')
+//   if (!authorElement) return
+//   if (authorElement.validity.valueMissing) return 'Please enter author of this post!'
+
+//   if (authorElement.value.split(' ').filter((x) => !!x && x.length >= 3).length < 2) {
+//     return 'Please enter at least two words of 3 character'
+//   }
+//   return ''
+// }
+
+function getPostSchema() {
+  return yup.object().shape({
+    title: yup.string().required('Please enter title'),
+    author: yup
+      .string()
+      .required('Please enter author')
+      .test(
+        'at-least-two words',
+        'Please enter at least two words',
+        (value) => value.split(' ').filter((x) => !!x && x.length >= 3).length >= 2
+      ),
+    description: yup.string(),
+  })
 }
 
-function getAuthorError(form){
-    const authorElement = form.querySelector('[name="author"]')
-    if(!authorElement) return
-    if(authorElement.validity.valueMissing) return 'Please enter author of this post!'
-
-    if(authorElement.value.split(' ').filter((x) => !! x && x.length >=3).length < 2){
-        return 'Please enter at least two words of 3 character'
-    }
-    return ''
+function setFieldError(form, name, error) {
+  const element = form.querySelector(`[name="${name}"]`)
+  if (element) {
+    element.setCustomValidity(error)
+    setTextContent(element.parentElement, '.invalid-feedback', error)
+  }
 }
 
+async function validatePostForm(form, formValues) {
+  //get errors
+  //   const errors = {
+  //     title: getTitleError(form),
+  //     author: getAuthorError(form),
+  //   }
+  //   //set errors
+  //   for (const key in errors) {
+  //     const element = form.querySelector(`[name="${key}"]`)
+  //     if (element) {
+  //       element.setCustomValidity(errors[key])
+  //       setTextContent(element.parentElement, '.invalid-feedback', errors[key])
+  //     }
+  //   }
+  try {
+    //reset previous errors
+    ;['title', 'author'].forEach((name) => setFieldError(form, name, ''))
+    //validation
+    const schema = getPostSchema()
+    await schema.validate(formValues, { abortEarly: false })
+  } catch (error) {
+    const errorLog = {}
 
-function validatePostForm(form, formValues){
-
-    //get errors
-    const errors = {
-        title: getTitleError(form),
-        author: getAuthorError(form),
+    if (error.name === 'ValidationError' && Array.isArray(error.inner)) {
+      for (const validationError of error.inner) {
+        const name = validationError.path
+        //ignore if the field is already logged
+        if (errorLog[name]) continue
+        //set field error and mark as logged
+        setFieldError(form, name, validationError.message)
+        errorLog[name] = true
+      }
     }
-    //set errors
-    for(const key in errors){
-     const element = form.querySelector(`[name="${key}"]`)
-     if(element){
-        element.setCustomValidity(errors[key])
-        setTextContent(element.parentElement, '.invalid-feedback', errors[key])
-     }   
-    }
-    //add was-validated class to form element
-    const isValid = form.checkValidity()
-    if(!isValid) form.classList.add('was-validated')
-    return false
+  }
+  //add was-validated class to form element
+  const isValid = form.checkValidity()
+  if (!isValid) form.classList.add('was-validated')
+  return false
 }
 
-export function initPostForm({formId, defaultValues, onSubmit}){
-    const form = document.getElementById(formId)
-    if(!form) return
-    console.log('form', form)
-    setFormValues(form, defaultValues)
+export function initPostForm({ formId, defaultValues, onSubmit }) {
+  const form = document.getElementById(formId)
+  if (!form) return
+  console.log('form', form)
+  setFormValues(form, defaultValues)
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
-        //get form value
-        const formValues = getFormValues(form)
-        console.log(formValues)
-        //validation js
-        if(!validatePostForm(form,formValues)) return
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    //get form value
+    const formValues = getFormValues(form)
+    console.log(formValues)
+    //validation js
+    if (!validatePostForm(form, formValues)) return
 
-        //trigger submit callback
+    //trigger submit callback
 
-        //otherwise, show validation errors
-        
-    })
+    //otherwise, show validation errors
+  })
 }
